e260c9c733a364677f28ee48c3dc1b9a75962660
Benjamin Mitzkus <b.mitzkus@gmx.de>
2024-11-20 17:43:01 +0100

    allow client gen skipping

--- config/config.go
+++ config/config.go
@@ -301,13 +301,14 @@ func (c *Config) loadLocalSchema() (*ast.Schema, error) {
 }
 
 type GenerateConfig struct {
-	Prefix            *NamingConfig `yaml:"prefix,omitempty"`
-	Suffix            *NamingConfig `yaml:"suffix,omitempty"`
-	GenerateInterface bool          `yaml:"generate_interface,omitempty"`
+	Prefix             *NamingConfig `yaml:"prefix,omitempty"`
+	Suffix             *NamingConfig `yaml:"suffix,omitempty"`
+	GenerateInterface  bool          `yaml:"generate_interface,omitempty"`
+	SkipGenerateClient bool          `yaml:"skip_generate_client,omitempty"`
 }
 
 func (c *GenerateConfig) ShouldGenerateClient() bool {
-	return true
+	return c != nil && !c.SkipGenerateClient
 }
 
 func (c *GenerateConfig) ShouldGenerateInterface() bool {
